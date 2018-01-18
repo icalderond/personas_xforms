bad1243c43b960d9cb997c93b77040999ffd47a8
icalderond <icalderond@outlook.com>
2018-01-17 20:19:53 -0600

    Generar proyecto Xamarin Forms

---
+++ personas/personasPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace personas
+{
+    public partial class personasPage : ContentPage
+    {
+        public personasPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
